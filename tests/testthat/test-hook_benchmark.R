166ab2505f3b4071a1cffc0ec54fb0147f73f2a6
atusy <30277794+atusy@users.noreply.github.com>
2020-07-23 22:19:39 +0900

    update tests

--- tests/testthat/test-hook_benchmark.R
+++ tests/testthat/test-hook_benchmark.R
@@ -1,17 +1,20 @@
+init <- function() {
+  knitr::opts_chunk$delete("benchmark")
+  knitr::knit_hooks$delete("benchmark")
+}
+
 test_that("Set chunk options and hooks", {
+  init()
   hook <- hook_benchmark()
-  testthat::expect_type(f, "closure")
+  testthat::expect_type(hook, "closure")
   expect_null(knitr::opts_chunk$get("benchmark"))
   expect_identical(knitr::knit_hooks$get("benchmark"), hook)
-  knitr::opts_chunk$restore()
-  knitr::knit_hooks$restore()
 })
 
 test_that("Skip set chunk options and hooks", {
+  init()
   hook <- hook_benchmark(default = TRUE, .set = FALSE)
   testthat::expect_type(hook, "closure")
   expect_null(knitr::opts_chunk$get("benchmark"))
   expect_null(knitr::knit_hooks$get("benchmark"))
-  knitr::opts_chunk$restore()
-  knitr::knit_hooks$restore()
 })
