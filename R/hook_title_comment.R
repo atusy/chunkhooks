b61857b095b2a76573d5a335df41c511dd0dcd2e
atusy <30277794+atusy@users.noreply.github.com>
2021-11-26 01:06:10 +0900

    add hook_title_comment

---
+++ R/hook_title_comment.R
@@ -0,0 +1,49 @@
+#' Prepend title comment to chunks 
+#'
+#' Title of chunks can be determined and prepended based on chunk options.
+#' 
+#' @param trigger A name of chunk option triggers the hook.
+#' @param default
+#'  A string or function as a default value of the `trigger`ing chunk option.
+#'  If string, the value is given to `glue::glue`, and evaluates the expression
+#'  by setting chunk options as an environment.
+#'  If function, it must have `options` parameter which receives chunk options,
+#'  and return a string.
+#' @param ...
+#'  Additional arguments given to `glue::glue()` or callable `default`.
+#' @param .set
+#'  `TRUE` or `FALSE` to set a default value and a hook to the `trigger`.
+#'
+#' @export
+hook_title_comment <- function(
+  trigger = "title",
+  default = "#' {label}.{engine}",
+  ...,
+  .set = TRUE
+) {
+  force(trigger)
+  args <- list(...)
+  hook <- function(options) {
+    title <- options[[trigger]]
+    options$code <- c(
+      do.call(
+        if (is.function(title)) {
+          function(...) title(options = options, ...)
+        } else {
+          function(...) glue::glue(title, .envir = options, ...)
+        },
+        args
+      ),
+      options$code
+    )
+    return(options)
+  }
+
+  if (.set) {
+    set(knitr::opts_chunk, trigger, list(default))
+    set(knitr::opts_hooks, trigger, list(hook))
+  }
+
+  return(invisible(hook))
+}
+
